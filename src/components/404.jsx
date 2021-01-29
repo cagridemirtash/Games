6c927a96f7b25459c0412ea6d6a9b8c27eb27a55
cagridemirtash <cagridemirtash@gmail.com>
2021-01-29 13:46:45 +0300

    :(

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 function NotFound(props) {
   const imgSource = `https://picsum.photos/id/289/400/300`;
   return (
-    <div className="mt-3">
-      <img src={imgSource} className="img-fluid" alt="" />
+    <div className="mt-3 text-light">
+      <img src={imgSource} className="" alt="" />
       <br />
       <br />
-      Something Wrong.
+      <p className="fs-2"> Opps, Something Wrong!!</p>
       <br />
       <br />
       <br />
