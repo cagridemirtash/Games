5004435864ffec41875d3cc296c0d862dcade2d2
cagridemirtash <cagridemirtash@gmail.com>
2021-01-29 13:25:23 +0300

    uptade

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -4,22 +4,17 @@ import { Link } from "react-router-dom";
 function NotFound(props) {
   const imgSource = `https://picsum.photos/id/289/400/300`;
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <div className="card">
-            <img src={imgSource} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Hop Hemşerim</h5>
-              <p className="card-text">Sen nereye geldiğini bilmiyorsun</p>
-              <button className="btn btn-success">
-                {" "}
-                <Link to="/">Geri bas lan</Link>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <br />
+      <br />
+      Something Wrong.
+      <br />
+      <br />
+      <br />
+      <br />
+      <Link to="/">
+        <button className="btn btn-danger">Go Home</button>
+      </Link>
     </div>
   );
 }
--- src/data/myData.js
+++ src/data/myData.js
@@ -9,7 +9,7 @@ const mydata = [
     {link: "/", title: "Home", component: Home,isExact: true, isLink : true},
     {link: "/games", title: "Games", component: Games,isExact: true,isLink : true},
     {link: "/games/:gameID", title: "All Games", component: GameDetail, isExact: false, isLink:false},
-    {link: "/search", title: "Arama", component: Search, isExact: false, isLink:false},
+    {link: "/search", title: "Search", component: Search, isExact: false, isLink:false},
     {link: "/contact", title: "Contact", component: Contact,isExact: true,isLink : true },
     {link: "/about", title: "About", component: About,isExact: true,isLink : true},
     {link: "", title: "404", component: NotFound, isExact: false, isLink:false}
